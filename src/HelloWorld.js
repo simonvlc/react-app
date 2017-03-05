054be5b4777925810238dd46fb6c8a3f558bfa2b
Simón Muñoz <simonvlc@gmail.com>
2017-03-05 15:08:26 +0100

    adding a remove button

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -7,10 +7,15 @@ class HelloWorld extends Component {
     super(props);
     this.state = { greeting: 'Hello' };
     this.frenchify = this.frenchify.bind(this);
+    this.removeGreeting = this.removeGreeting.bind(this);
   }
 
   frenchify(){
     this.setState({ greeting: 'Bonjour' });
+  }
+
+  removeGreeting() {
+    this.props.removeGreeting(this.props.name);
   }
 
   render(){
@@ -19,6 +24,8 @@ class HelloWorld extends Component {
         {this.state.greeting} {this.props.name}!
         <br/>
         <button onClick={this.frenchify}>Frenchify!</button>
+        <br/>
+        <button onClick={this.removeGreeting}>Remove Me!</button>
       </div>
     );
   }
--- src/HelloWorldList.js
+++ src/HelloWorldList.js
@@ -9,15 +9,27 @@ class HelloWorldList extends Component {
     super(props);
     this.state = { greetings: ['Jim', 'Sally', 'Bender'] };
     this.addGreeting = this.addGreeting.bind(this);
+    this.removeGreeting = this.removeGreeting.bind(this);
   }
 
   addGreeting(newName) {
     this.setState({ greetings: [...this.state.greetings, newName] });
   }
 
+  removeGreeting(removeName) {
+    const filteredGreetings = this.state.greetings.filter(name => {
+      return name !== removeName;
+    });
+    this.setState({ greetings: filteredGreetings });
+  }
+
   renderGreetings() {
     return this.state.greetings.map(name => (
-      <HelloWorld key={name} name={name}/>
+      <HelloWorld
+        key={name}
+        name={name}
+        removeGreeting={this.removeGreeting}
+      />
     ));
   }
 
