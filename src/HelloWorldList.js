5d2b81686545d369f8a98c4597ff77f436d0404a
Simón Muñoz <simonvlc@gmail.com>
2017-03-05 15:01:55 +0100

    creating an input component

--- src/HelloWorldList.js
+++ src/HelloWorldList.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './HelloWorldList.css';
 import HelloWorld from './HelloWorld';
+import AddGreeter from './AddGreeter';
 
 class HelloWorldList extends Component {
 
   constructor(props) {
     super(props);
     this.state = { greetings: ['Jim', 'Sally', 'Bender'] };
+    this.addGreeting = this.addGreeting.bind(this);
+  }
+
+  addGreeting(newName) {
+    this.setState({ greetings: [...this.state.greetings, newName] });
   }
 
   renderGreetings() {
@@ -18,6 +24,7 @@ class HelloWorldList extends Component {
   render () {
     return (
       <div className="HelloWorldList">
+        <AddGreeter addGreeting={this.addGreeting}/>
         {this.renderGreetings()}
       </div>
     )
