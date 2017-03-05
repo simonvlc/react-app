89b5207e9d8994e9734153481ea89d08855a227c
Simón Muñoz <simonvlc@gmail.com>
2017-03-05 13:50:41 +0100

    refactoring to first component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import HelloWorld from './HelloWorld';
 
 const App = () => {
-  return (<div className="App">Hello World!</div>);
-}
+  return (
+    <div className="App">
+      <HelloWorld/>
+      <HelloWorld/>
+      <HelloWorld/>
+    </div>
+  );
+};
 
 export default App;
