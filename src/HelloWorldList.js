0fde916bcea7e80c55eedfeeb20734178d654936
Simón Muñoz <simonvlc@gmail.com>
2017-03-05 14:46:43 +0100

    creating a helper function

--- src/HelloWorldList.js
+++ src/HelloWorldList.js
@@ -3,11 +3,22 @@ import './HelloWorldList.css';
 import HelloWorld from './HelloWorld';
 
 class HelloWorldList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { greetings: ['Jim', 'Sally', 'Bender'] };
+  }
+
+  renderGreetings() {
+    return this.state.greetings.map(name => (
+      <HelloWorld key={name} name={name}/>
+    ));
+  }
+
   render () {
     return (
       <div className="HelloWorldList">
-        <HelloWorld name="Jim"/>
-        <HelloWorld name="Sally"/>
+        {this.renderGreetings()}
       </div>
     )
   }
